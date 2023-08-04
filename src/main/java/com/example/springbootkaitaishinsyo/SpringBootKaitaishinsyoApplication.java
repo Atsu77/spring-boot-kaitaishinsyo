c5f82aabe3f38f304c2d0e2da38735124e7e162c
Atsu77 <kamnknon@gmail.com>
2023-08-04 20:26:02 +0900

    First commit

---
+++ src/main/java/com/example/springbootkaitaishinsyo/SpringBootKaitaishinsyoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootkaitaishinsyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKaitaishinsyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKaitaishinsyoApplication.class, args);
+	}
+
+}
